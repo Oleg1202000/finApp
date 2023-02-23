87637bc2c2f0283d8967a6336cc031f9e5b6a904
Oleg1202000 <Oleg1202000@gmail.com>
2023-02-23 20:40:03 +0300

    добавил индексы в categories и tags

--- mobile/app/src/main/java/com/oleg1202000/finapp/data/Categories.kt
+++ mobile/app/src/main/java/com/oleg1202000/finapp/data/Categories.kt
@@ -2,16 +2,17 @@ package com.oleg1202000.finapp.data
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
 @Entity(
-    tableName = "categories"
+    tableName = "categories" ,
+    indices = [
+        Index("name", unique = true)
+    ]
+
 )
 data class Categories(
     @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id") val id: ULong,
     @ColumnInfo(name = "name") val name: String
-
-    // добовить уникальные индексы в справочники
-
-
 )
