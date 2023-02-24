07b2ec192a258e095cdc503330ca45416d1ff1c2
Oleg1202000 <Oleg1202000@gmail.com>
2023-02-24 22:30:51 +0300

    добавил DatabaseBuilder

--- mobile/app/src/main/java/com/oleg1202000/finapp/data/FinappDatabase.kt
+++ mobile/app/src/main/java/com/oleg1202000/finapp/data/FinappDatabase.kt
@@ -2,22 +2,19 @@ package com.oleg1202000.finapp.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.oleg1202000.finapp.data.dao.CategoriesDao
+import com.oleg1202000.finapp.data.dao.TagsDao
+
 
 @Database (
     entities = [
-        Users::class,
-        Targets::class,
         Categories::class,
-        SubCategories:: class,
-        Tags::class,
-        Accounts::class,
-        SummaryTags::class,
-        SubCategoriesCategories::class,
-        UsersCategories::class
+        Tags::class
     ],
     version = 1,
     exportSchema = false
-        )
+)
 abstract class FinappDatabase : RoomDatabase() {
-    // abstract fun usersDAO(): UsersDAO
+    abstract fun categoriesDAO(): CategoriesDao
+    abstract fun tagsDAO(): TagsDao
 }
