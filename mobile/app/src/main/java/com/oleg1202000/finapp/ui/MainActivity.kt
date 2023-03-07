9bd72e62026b8c118696f16edd050d78a201dce7
Oleg1202000 <Oleg1202000@gmail.com>
2023-03-07 21:58:15 +0300

    Мелкие изменения в MianActivity

--- mobile/app/src/main/java/com/oleg1202000/finapp/ui/MainActivity.kt
+++ mobile/app/src/main/java/com/oleg1202000/finapp/ui/MainActivity.kt
@@ -3,46 +3,14 @@ package com.oleg1202000.finapp.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.oleg1202000.finapp.ui.theme.Fin_appTheme
-
-
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Fin_appTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("World")
-                }
-            }
+            Finapp()
         }
 
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Fin_appTheme {
-        Greeting("Android")
     }
 }
