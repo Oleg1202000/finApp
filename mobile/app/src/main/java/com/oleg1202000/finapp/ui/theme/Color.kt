9f3910554f560bf35b44cb4f6d85a7f038c0c071
Oleg1202000 <oleg1202000@gmail.come>
2023-03-21 00:24:42 +0300

    Изменения внешнего вида приложения (изменение темы)

--- mobile/app/src/main/java/com/oleg1202000/finapp/ui/theme/Color.kt
+++ mobile/app/src/main/java/com/oleg1202000/finapp/ui/theme/Color.kt
@@ -2,7 +2,9 @@ package com.oleg1202000.finapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
+
+
 val Teal200 = Color(0xFF03DAC5)
+val cerulean = Color(0xFF31E2F8)
+val LightGray= Color(0xFFC0D1CF)
+val DarkGrey= Color(0xFF151719)
--- mobile/app/src/main/java/com/oleg1202000/finapp/ui/theme/Theme.kt
+++ mobile/app/src/main/java/com/oleg1202000/finapp/ui/theme/Theme.kt
@@ -1,21 +1,25 @@
 package com.oleg1202000.finapp.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = DarkGrey,
+    primaryVariant = Color.Black,
+    secondary = Teal200,
+    background = Color.Black
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = LightGray,
+    primaryVariant = Color.White,
+    secondary = cerulean,
+    background = Color.White,
 
     /* Other default colors to override
     background = Color.White,
@@ -29,15 +33,19 @@ private val LightColorPalette = lightColors(
 
 @Composable
 fun FinappTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
+    val colors: Colors
+    val typography: androidx.compose.material.Typography
+    if (darkTheme) {
+            colors = DarkColorPalette
+            typography = Typography
     } else {
-        LightColorPalette
+            colors = LightColorPalette
+            typography = Typography
     }
 
     MaterialTheme(
         colors = colors,
-        typography = Typography,
+        typography = typography,
         shapes = Shapes,
         content = content
     )
