6f6af3d65787aa436267b474a9abdf693e26ed7e
Oleg1202000 <oleg1202000@gmail.come>
2023-03-26 16:07:58 +0300

    добавил таблицу "Plan"

--- mobile/app/src/main/java/com/oleg1202000/finapp/data/FinappDatabase.kt
+++ mobile/app/src/main/java/com/oleg1202000/finapp/data/FinappDatabase.kt
@@ -1,9 +1,14 @@
 package com.oleg1202000.finapp.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.oleg1202000.finapp.data.dao.CategoriesDao
+import com.oleg1202000.finapp.data.dao.PlanDAO
+import com.oleg1202000.finapp.data.dao.SummaryDao
 import com.oleg1202000.finapp.data.dao.TagsDao
+
 
 
 @Database (
@@ -17,4 +22,12 @@ import com.oleg1202000.finapp.data.dao.TagsDao
 abstract class FinappDatabase : RoomDatabase() {
     abstract fun categoriesDAO(): CategoriesDao
     abstract fun tagsDAO(): TagsDao
+    abstract fun summaryDAO(): SummaryDao
+
+    abstract fun planDAO(): PlanDAO
 }
+
+private lateinit var applicationContext: Context
+val db = Room.databaseBuilder(
+    context = applicationContext, FinappDatabase::class.java, "finappDatabase.db"
+).build()
---
+++ mobile/app/src/main/java/com/oleg1202000/finapp/data/dao/PlanDAO.kt
@@ -0,0 +1,27 @@
+package com.oleg1202000.finapp.data.dao
+
+import androidx.room.Insert
+import androidx.room.Query
+import com.oleg1202000.finapp.data.Plan
+import com.oleg1202000.finapp.data.ReturnPlanName
+
+interface PlanDAO {
+    @Query(
+        """
+        SELECT categories.name, plan.limit
+        
+        FROM plan
+        
+        JOIN categories ON categories.id = plan.category_id
+        
+        WHERE plan.date >= :beginDate AND plan.date <= :endDate
+        
+        GROUP BY plan.category_id
+        """
+    )
+    fun getPlan(beginDate: String,
+                endDate: String): List<ReturnPlanName>
+
+    @Insert
+    fun addPlan(plan: Plan)
+}
