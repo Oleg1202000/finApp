0bd33c0713f567b064f5a369cb3bc494c02be8be
Oleg1202000 <oleg1202000@gmail.come>
2023-04-24 01:47:24 +0300

    изменения в SystemBar и FloatingActionBar

--- mobile/app/src/main/java/com/oleg1202000/finapp/ui/FinApp.kt
+++ mobile/app/src/main/java/com/oleg1202000/finapp/ui/FinApp.kt
@@ -5,11 +5,21 @@ package com.oleg1202000.finapp.ui
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavDestination
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.oleg1202000.finapp.ui.components.FinappFloatingActionButton
 import com.oleg1202000.finapp.ui.components.FinappNavigationBar
+import com.oleg1202000.finapp.ui.components.ScreenItem
+import com.oleg1202000.finapp.ui.components.StatusBar
+import com.oleg1202000.finapp.ui.components.navItems
 import com.oleg1202000.finapp.ui.theme.FinappTheme
 import com.oleg1202000.finapp.ui.theme.Typography
 
@@ -20,40 +30,54 @@ fun Finapp() {
 
     FinappTheme {
 
+        val showSystemBar: MutableState<Boolean> = remember {
+            mutableStateOf(true)
+        }
+        val showFloatingActionButton: MutableState<Boolean> = remember {
+            mutableStateOf(true)
+        }
+
+        val statusBarItem: MutableState<String> = remember {
+            mutableStateOf("")
+        }
+
+
+
         val navController = rememberNavController()
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentDestination = navBackStackEntry?.destination
 
         Scaffold(
-            topBar = { StatusBar()},
-            bottomBar = { FinappNavigationBar(navController = navController) },
+           topBar = {if (showSystemBar.value)
+                StatusBar(
+                    item = statusBarItem,  // Для отображения суммы доходов и расходов
+                )
+                    },
+
+            bottomBar = {if (showSystemBar.value)
+                FinappNavigationBar(
+                navController = navController,
+                currentDestination = currentDestination,
+            )
+                        },
+
+        floatingActionButton = { if (showSystemBar.value && showFloatingActionButton.value) {
+            FinappFloatingActionButton(
+                navController = navController,
+                currentDestination = currentDestination
+            )
+        }
+        }
         ) {
             padding ->
             Box(modifier = Modifier.padding(padding)) {
-                NavGraph(navController = navController)
+                NavGraph(
+                    navController = navController,
+                    showSystemBar = showSystemBar,
+                    showFloatingActionButton = showFloatingActionButton,
+                    statusBarItem = statusBarItem
+                )
             }
         }
     }
 }
-
-
-@Composable
-fun StatusBar(){
-
-    TopAppBar(
-        title = {
-            Row(
-                modifier = Modifier.fillMaxSize(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text(text ="[Название экрана]", style = Typography.titleLarge)
-                Text(text ="Настройки", style = Typography.labelMedium)
-            }
-        },
-        windowInsets = WindowInsets.statusBars,
-        modifier = Modifier.height(60.dp)
-    )
-}
-
-
-
-
---
+++ mobile/app/src/main/java/com/oleg1202000/finapp/ui/components/FloatingActionButton.kt
@@ -0,0 +1,34 @@
+package com.oleg1202000.finapp.ui.components
+
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.NavDestination
+import androidx.navigation.NavHostController
+import com.oleg1202000.finapp.R
+import com.oleg1202000.finapp.ui.Screen
+
+
+@Composable
+fun FinappFloatingActionButton(
+    navController: NavHostController,
+    currentDestination : NavDestination?
+) {
+
+    FloatingActionButton(
+        onClick = {
+
+            if (currentDestination?.route == "home") {
+                navController.navigate(Screen.AddData.route)
+            }
+            else if (currentDestination?.route == "plan") {
+                navController.navigate(Screen.AddPlan.route)
+            }
+        }
+    ) {
+        Icon(painter = painterResource(id = R.drawable.ic_add_40px), contentDescription = "Add expense / plan")
+    }
+
+
+}
