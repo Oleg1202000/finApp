77eaa675650c91473a1163bd391820e53cbc9a8b
Oleg1202000 <oleg1202000@gmail.come>
2023-04-24 01:49:31 +0300

    изменил некоторые цвета

--- mobile/app/src/main/java/com/oleg1202000/finapp/ui/theme/Color.kt
+++ mobile/app/src/main/java/com/oleg1202000/finapp/ui/theme/Color.kt
@@ -6,6 +6,13 @@ import androidx.compose.ui.graphics.Color
 
 val Teal200 = Color(0xFF03DAC5)
 val cerulean = Color(0xFF31E2F8)
-val SurfaceLightGray= Color(0xFFC0D1CF)
-val PrimaryLightGray= Color(0xFF4E5353)
+val SurfaceLightGray= Color(0xFFD1F0EC)
+val PrimaryLightGray= Color(0xFFB0E9E9)
 val DarkGrey= Color(0xFF151719)
+
+
+// Цвета для графиков
+val defaultColor = Color(0xFFA6ACAA)
+val okColor = Color(0xFF43A047)
+val notOkColor = Color(0xFFE53935)
+val notOk80Color = Color(0xFFFB8C00)  // 80 out of 100 %
