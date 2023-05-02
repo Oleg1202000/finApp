278d238277a297d003f0ec6709317bb5ba549ec5
Oleg1202000 <oleg1202000@gmail.come>
2023-05-03 00:35:44 +0300

    Изменил тему

--- mobile/app/src/main/java/com/oleg1202000/finapp/ui/theme/Color.kt
+++ mobile/app/src/main/java/com/oleg1202000/finapp/ui/theme/Color.kt
@@ -2,13 +2,70 @@ package com.oleg1202000.finapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val md_theme_light_primary = Color(0xFF005BBD)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_primaryContainer = Color(0xFFD7E2FF)
+val md_theme_light_onPrimaryContainer = Color(0xFF001A40)
+val md_theme_light_secondary = Color(0xFF006E2E)
+val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+val md_theme_light_secondaryContainer = Color(0xFF94F8A3)
+val md_theme_light_onSecondaryContainer = Color(0xFF002109)
+val md_theme_light_tertiary = Color(0xFF8C5000)
+val md_theme_light_onTertiary = Color(0xFFFFFFFF)
+val md_theme_light_tertiaryContainer = Color(0xFFFFDCBF)
+val md_theme_light_onTertiaryContainer = Color(0xFF2D1600)
+val md_theme_light_error = Color(0xFFBA1A1A)
+val md_theme_light_errorContainer = Color(0xFFFFDAD6)
+val md_theme_light_onError = Color(0xFFFFFFFF)
+val md_theme_light_onErrorContainer = Color(0xFF410002)
+val md_theme_light_background = Color(0xFFFFFBFF)
+val md_theme_light_onBackground = Color(0xFF1B0261)
+val md_theme_light_surface = Color(0xFFFFFBFF)
+val md_theme_light_onSurface = Color(0xFF1B0261)
+val md_theme_light_surfaceVariant = Color(0xFFE0E2EC)
+val md_theme_light_onSurfaceVariant = Color(0xFF44474E)
+val md_theme_light_outline = Color(0xFF74777F)
+val md_theme_light_inverseOnSurface = Color(0xFFF4EEFF)
+val md_theme_light_inverseSurface = Color(0xFF302175)
+val md_theme_light_inversePrimary = Color(0xFFACC7FF)
+val md_theme_light_shadow = Color(0xFF000000)
+val md_theme_light_surfaceTint = Color(0xFF005BBD)
+val md_theme_light_outlineVariant = Color(0xFFC4C6D0)
+val md_theme_light_scrim = Color(0xFF000000)
+
+val md_theme_dark_primary = Color(0xFFACC7FF)
+val md_theme_dark_onPrimary = Color(0xFF002F67)
+val md_theme_dark_primaryContainer = Color(0xFF004491)
+val md_theme_dark_onPrimaryContainer = Color(0xFFD7E2FF)
+val md_theme_dark_secondary = Color(0xFF78DB8A)
+val md_theme_dark_onSecondary = Color(0xFF003915)
+val md_theme_dark_secondaryContainer = Color(0xFF005321)
+val md_theme_dark_onSecondaryContainer = Color(0xFF94F8A3)
+val md_theme_dark_tertiary = Color(0xFFFFB872)
+val md_theme_dark_onTertiary = Color(0xFF4A2800)
+val md_theme_dark_tertiaryContainer = Color(0xFF6A3B00)
+val md_theme_dark_onTertiaryContainer = Color(0xFFFFDCBF)
+val md_theme_dark_error = Color(0xFFFFB4AB)
+val md_theme_dark_errorContainer = Color(0xFF93000A)
+val md_theme_dark_onError = Color(0xFF690005)
+val md_theme_dark_onErrorContainer = Color(0xFFFFDAD6)
+val md_theme_dark_background = Color(0xFF1B0261)
+val md_theme_dark_onBackground = Color(0xFFE5DEFF)
+val md_theme_dark_surface = Color(0xFF1B0261)
+val md_theme_dark_onSurface = Color(0xFFE5DEFF)
+val md_theme_dark_surfaceVariant = Color(0xFF44474E)
+val md_theme_dark_onSurfaceVariant = Color(0xFFC4C6D0)
+val md_theme_dark_outline = Color(0xFF8E9099)
+val md_theme_dark_inverseOnSurface = Color(0xFF1B0261)
+val md_theme_dark_inverseSurface = Color(0xFFE5DEFF)
+val md_theme_dark_inversePrimary = Color(0xFF005BBD)
+val md_theme_dark_shadow = Color(0xFF000000)
+val md_theme_dark_surfaceTint = Color(0xFFACC7FF)
+val md_theme_dark_outlineVariant = Color(0xFF44474E)
+val md_theme_dark_scrim = Color(0xFF000000)
 
 
-val Teal200 = Color(0xFF03DAC5)
-val cerulean = Color(0xFF31E2F8)
-val SurfaceLightGray= Color(0xFFD1F0EC)
-val PrimaryLightGray= Color(0xFFB0E9E9)
-val DarkGrey= Color(0xFF151719)
+val seed = Color(0xFF0065D0)
 
 
 // Цвета для графиков
@@ -16,3 +73,80 @@ val defaultColor = Color(0xFFA6ACAA)
 val okColor = Color(0xFF43A047)
 val notOkColor = Color(0xFFE53935)
 val notOk80Color = Color(0xFFFB8C00)  // 80 out of 100 %
+
+
+// Цвета для категорий
+private val Red500 = Color(0xFFF44336)
+private val RedA400 = Color(0xFFFF1744)
+private val Pink500 = Color(0xFFE91E63)
+private val PinkA400 = Color(0xFFF50057)
+private val Purple500 = Color(0xFF9C27B0)
+private val PurpleA400 = Color(0xFFD500F9)
+private val Deep_Purple500 = Color(0xFF673AB7)
+private val Deep_PurpleA400 = Color(0xFF651FFF)
+private val Indigo500 = Color(0xFF3F51B5)
+private val IndigoA400 = Color(0xFF3D5AFE)
+private val Blue500 = Color(0xFF2196F3)
+private val BlueA400 = Color(0xFF2979FF)
+private val Light_Blue500 = Color(0xFF00BCD4)
+private val Light_BlueA400 = Color(0xFF00B0FF)
+private val Cyan500 = Color(0xFF00BCD4)
+private val CyanA400 = Color(0xFF00E5FF)
+private val Teal500 = Color(0xFF009688)
+private val TealA400 = Color(0xFF1DE9B6)
+private val Green500 = Color(0xFF4CAF50)
+private val GreenA400 = Color(0xFF00E676)
+private val Light_Green500 = Color(0xFF8BC34A)
+private val Light_GreenA400 = Color(0xFF76FF03)
+private val Lime500 = Color(0xFFCDDC39)
+private val LimeA400 = Color(0xFFC6FF00)
+private val Yellow500 = Color(0xFFFFEB3B)
+private val YellowA400 = Color(0xFFFFEA00)
+private val Amber500 = Color(0xFFFFC107)
+private val AmberA400 = Color(0xFFFFC400)
+private val Orange500 = Color(0xFFFF9800)
+private val OrangeA400 = Color(0xFFFF9100)
+private val Deep_Orange500 = Color(0xFFFF5722)
+private val Deep_OrangeA400 = Color(0xFFFF3D00)
+private val Brown500 = Color(0xFF795548)
+private val Gray500 = Color(0xFF9E9E9E)
+private val Blue_Gray500 = Color(0xFF607D8B)
+
+
+val colorCategories: List<Color> = listOf(
+    Red500,
+    RedA400,
+    Pink500,
+    PinkA400,
+    Purple500,
+    PurpleA400,
+    Deep_Purple500,
+    Deep_PurpleA400,
+    Indigo500,
+    IndigoA400,
+    Blue500,
+    BlueA400,
+    Light_Blue500,
+    Light_BlueA400,
+    Cyan500,
+    CyanA400,
+    Teal500,
+    TealA400,
+    Green500,
+    GreenA400,
+    Light_Green500,
+    Light_GreenA400,
+    Lime500,
+    LimeA400,
+    Yellow500,
+    YellowA400,
+    Amber500,
+    AmberA400,
+    Orange500,
+    OrangeA400,
+    Deep_Orange500,
+    Deep_OrangeA400,
+    Brown500,
+    Gray500,
+    Blue_Gray500
+)
