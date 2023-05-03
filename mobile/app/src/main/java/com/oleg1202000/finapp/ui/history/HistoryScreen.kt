550b4d33f8e4cde6031a6dbaa22d20bd0e2ebe20
Oleg1202000 <oleg1202000@gmail.come>
2023-05-04 02:44:54 +0300

    Перенес Scafold в FinApp.kt

--- mobile/app/src/main/java/com/oleg1202000/finapp/ui/history/HistoryScreen.kt
+++ mobile/app/src/main/java/com/oleg1202000/finapp/ui/history/HistoryScreen.kt
@@ -1,43 +1,23 @@
 package com.oleg1202000.finapp.ui.history
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.navigation.NavDestination
 import androidx.navigation.NavHostController
-import com.oleg1202000.finapp.ui.components.FinappNavigationBar
-import com.oleg1202000.finapp.ui.components.FinappStatusBar
 
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HistoryScreen(
     navController: NavHostController,
     currentDestination: NavDestination?
 ) {
-    Scaffold(
 
-        topBar = {
-            FinappStatusBar()
-        },
-
-        bottomBar = {
-            FinappNavigationBar(
-                navController = navController,
-                currentDestination = currentDestination,
-            )
-        },
-
-    ) { innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding)) {
+        Column {
 
 
             Text(text = "History")
 
         }
-    }
+
 }
