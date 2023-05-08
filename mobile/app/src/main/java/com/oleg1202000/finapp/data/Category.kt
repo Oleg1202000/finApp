5512273e5218aebd7e4ce8fd47384ddcb65dcee5
Oleg1202000 <--global>
2023-05-09 00:17:16 +0300

    Добавил обработку ошибок в AddCategoryScreen

--- mobile/app/src/main/java/com/oleg1202000/finapp/data/Category.kt
+++ mobile/app/src/main/java/com/oleg1202000/finapp/data/Category.kt
@@ -2,13 +2,14 @@ package com.oleg1202000.finapp.data
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
 @Entity(
     tableName = "categories" ,
-   /* indices = [
+    indices = [
         Index("name", unique = true)
-    ]*/
+    ]
 
 )
 data class Category(
