361238fcda7eaddf6386430bd0051d5935d36b91
Oleg1202000 <--global>
2023-05-09 21:50:24 +0300

    Реализовал экран Plan

--- mobile/app/src/main/java/com/oleg1202000/finapp/ui/plan/PlanScreen.kt
+++ mobile/app/src/main/java/com/oleg1202000/finapp/ui/plan/PlanScreen.kt
@@ -1,22 +1,86 @@
 package com.oleg1202000.finapp.ui.plan
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavDestination
-import androidx.navigation.NavHostController
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.oleg1202000.finapp.ui.graphdraw.BarGraph
+import com.oleg1202000.finapp.ui.graphdraw.ButtonGraph
+import com.oleg1202000.finapp.ui.graphdraw.TableAmount
 
 
 @Composable
 fun PlanScreen(
-    navController: NavHostController,
-    currentDestination: NavDestination?,
+    viewModel: PlanViewModel = viewModel(),
+    finappStatusBarTitle: MutableState<String>
 ) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
 
-        Column {
-            Text(text = "Plan")
+    LazyColumn {
 
+        // график 1
+        item {
+            if (uiState.dataGraph.isEmpty()) {
+                finappStatusBarTitle.value = "- 0   + 0" // Для отображения суммы доходов и расходов
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(
+                            top = 300.dp
+                        ),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(text = "Ничего не запланировано")
+                }
+            } else {
+
+                finappStatusBarTitle.value = "${uiState.sumFact} / ${uiState.sumPlanned} ₽"
+                BarGraph(
+                    dataGraph = uiState.dataGraph
+                )
+            }
         }
 
+
+        item { Spacer(modifier = Modifier.height(20.dp)) }
+
+        item {
+            ButtonGraph(
+                beginDate = uiState.beginDate,
+                endDate = uiState.endDate,
+                updateDate = { viewModel.getDate(it) },
+                updateDataGraph = { viewModel.updateDataGraph() },
+                updateGraphPeriod = { viewModel.updateGraphPeriod(it) }
+            )
+        }
+
+        item { Spacer(modifier = Modifier.height(20.dp)) }
+
+        // "таблица" из категорий и трат
+        item {
+            TableAmount(
+                dataGraph = uiState.dataGraph
+            )
+        }
+
+        item {
+            Spacer(
+                modifier = Modifier.height(80.dp) // Чтобы кнопка "+" не закрывала контент
+            )
+        }
+    }
 }
