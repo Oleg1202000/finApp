ebd6d2bc6b99a8bb5dc3f15e2ceedc58bb62826b
Oleg1202000 <--global>
2023-05-09 21:41:19 +0300

    Добавил DataStore в di

--- mobile/app/src/main/java/com/oleg1202000/finapp/di/RepositoryModule.kt
+++ mobile/app/src/main/java/com/oleg1202000/finapp/di/RepositoryModule.kt
@@ -1,19 +1,30 @@
 package com.oleg1202000.finapp.di
 
-import com.oleg1202000.finapp.data.*
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.longPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
+import com.oleg1202000.finapp.data.Category
+import com.oleg1202000.finapp.data.Planned
+import com.oleg1202000.finapp.data.Summary
 import com.oleg1202000.finapp.data.dao.CategoriesDao
 import com.oleg1202000.finapp.data.dao.PlanDao
 import com.oleg1202000.finapp.data.dao.SummaryDao
+import com.oleg1202000.finapp.ui.graphdraw.GraphPeriod
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 
- class LocalRepositoryModule @Inject constructor(
+class LocalRepositoryModule @Inject constructor(
 
     private val categoriesDao: CategoriesDao,
     private val summaryDao: SummaryDao,
-    private val planDao: PlanDao
+    private val planDao: PlanDao,
+    private val dataPreferences: DataStore<Preferences>
  ) {
 
 
@@ -55,4 +66,37 @@ import javax.inject.Inject
      suspend fun deleteSummary(id: Long) = summaryDao.deleteSummaryById(id)
 
      suspend fun updateSummary(summary: Summary) = summaryDao.updateSummary(summary)
+
+
+    // DataStore
+     fun getDatePeriod(datePeriodKey: Preferences.Key<String> = stringPreferencesKey("plan_date_period")): Flow<GraphPeriod> = dataPreferences.data
+         .map { preferences ->
+
+             when (preferences[datePeriodKey]) {
+                 "DAY" -> GraphPeriod.DAY
+                 "WEEK" -> GraphPeriod.WEEK
+                 else -> GraphPeriod.MONTH
+
+             }
+
+         }
+
+    fun getLastUpdateDate(lastUpdateMillisKey: Preferences.Key<Long> = longPreferencesKey("last_update_millis")): Flow<Long> = dataPreferences.data
+        .map { preferences ->
+            // No type safety.
+            preferences[lastUpdateMillisKey] ?: 0
+        }
+
+    suspend fun setDatePeriod(datePeriod: String) {
+        dataPreferences.edit { plan ->
+            plan[stringPreferencesKey("plan_date_period")] = datePeriod
+        }
+    }
+
+    suspend fun setLastUpdateDate(lastUpdateMillis: Long) {
+        dataPreferences.edit { plan ->
+            plan[longPreferencesKey("last_update_millis")] = lastUpdateMillis
+        }
+    }
+
 }
