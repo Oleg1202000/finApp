da6385700753d0a4b9846e795f6d219df0153af3
Oleg1202000 <--global>
2023-05-11 01:00:27 +0300

    Изменил отображение надписи "Нет записей"

--- mobile/app/src/main/java/com/oleg1202000/finapp/ui/plan/PlanScreen.kt
+++ mobile/app/src/main/java/com/oleg1202000/finapp/ui/plan/PlanScreen.kt
@@ -5,8 +5,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
@@ -35,16 +36,16 @@ fun PlanScreen(
         item {
             if (uiState.dataGraph.isEmpty()) {
                 finappStatusBarTitle.value = "0 / 0 ₽" // Для отображения суммы доходов и расходов
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(
-                            top = 300.dp
-                        ),
-                    horizontalArrangement = Arrangement.Center,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(text = "Ничего не запланировано")
+                Card {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .size(300.dp),
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(text = "Ничего не запланировано")
+                    }
                 }
             } else {
 
