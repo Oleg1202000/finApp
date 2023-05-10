8d28f595afec396a2ebf95e213138f86c5e8d103
Oleg1202000 <--global>
2023-05-11 01:31:02 +0300

    Закомментировал кнопку настроек

--- mobile/app/src/main/java/com/oleg1202000/finapp/ui/components/FinappStatusBar.kt
+++ mobile/app/src/main/java/com/oleg1202000/finapp/ui/components/FinappStatusBar.kt
@@ -5,18 +5,14 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import com.oleg1202000.finapp.R
 import com.oleg1202000.finapp.ui.theme.Typography
 
 
@@ -43,13 +39,13 @@ fun FinappStatusBar(
             }
         },
 
-        actions = {
+        /*actions = {
                 Icon(
                     modifier = Modifier.padding(10.dp),
                     painter = painterResource(id = R.drawable.ic_settings_40px),
                     contentDescription = "Settings"
                 )
-        },
+        },*/
 
         windowInsets = WindowInsets.statusBars
     )
