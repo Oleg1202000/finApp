d8914c51c8641cdc0531bc91b2bf3733ea206a36
Oleg1202000 <Oleg1202000@gmail.com>
2023-09-23 13:12:29 +0300

    create fake data repository

---
+++ app/src/test/java/com/oleg1202000/finapp/data/FakeData.kt
@@ -0,0 +1,95 @@
+package com.oleg1202000.finapp.data
+
+import com.oleg1202000.finapp.di.IRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class FakeData : IRepository {
+
+    private var fakeCategories: MutableList<Category> = mutableListOf()
+    var fakeSummary: MutableList<Summary> = mutableListOf()
+
+    fun initCategories(categories: List<Category>){
+        fakeCategories.addAll(categories)
+
+        // TODO: заменить на setCategory
+    }
+
+
+    override fun getCategories(isIncome: Boolean): Flow<List<CategoryWithoutIsIncome>> =
+        flow {
+            emit(
+                fakeCategories.map {
+                    CategoryWithoutIsIncome(
+                        id = it.id,
+                        name = it.name,
+                        color = it.color,
+                        iconId = it.iconId,
+                    )
+                }
+            )
+        }
+
+    override suspend fun setCategory(category: Category) {
+        fakeCategories.add(category)
+    }
+
+    override suspend fun deleteCategoryById(id: Long) {
+        fakeCategories.remove(
+            fakeCategories.find {
+                it.id == id
+            }
+        )
+    }
+
+    override fun getPlan(
+        isIncome: Boolean,
+        beginDate: Long,
+        endDate: Long
+    ): Flow<List<ReturnPlanAmount>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun setPlan(plan: Planned) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deletePlanById(id: Long) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getPlannedHistory(
+        beginDate: Long,
+        endDate: Long
+    ): Flow<List<ReturnPlannedHistory>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getSumAmount(
+        isIncome: Boolean,
+        beginDate: Long,
+        endDate: Long
+    ): Flow<List<ReturnSumAmount>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getSummaryHistory(
+        beginDate: Long,
+        endDate: Long
+    ): Flow<List<ReturnSummaryHistory>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun setSummary(summary: Summary) {
+        fakeSummary.add(summary)
+    }
+
+    override suspend fun deleteSummaryById(id: Long) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun updateSummary(summary: Summary) {
+        TODO("Not yet implemented")
+    }
+
+}
